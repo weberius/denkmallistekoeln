2fcc3d0a4d91fde3efd9a3598d2c445412927c77
Wolfram <eberius@gmail.com>
2015-06-10 06:06:23 +0200

    implement Facade

--- src/main/java/de/illilli/opendata/service/denkmallistekoeln/DenkmallisteKoelnFacade.java
+++ src/main/java/de/illilli/opendata/service/denkmallistekoeln/DenkmallisteKoelnFacade.java
@@ -1,12 +1,40 @@
 package de.illilli.opendata.service.denkmallistekoeln;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import de.illilli.opendata.koeln.json.AskForDenkmallisteKoeln;
+import de.illilli.opendata.service.denkmallistekoeln.json.Denkmal;
+
 public class DenkmallisteKoelnFacade implements Facade {
 
-	private StringBuilder sb = new StringBuilder("{}");
+	private static final Logger logger = Logger
+			.getLogger(DenkmallisteKoelnFacade.class);
+
+	private StringBuilder json = new StringBuilder("{}");
+
+	public DenkmallisteKoelnFacade() {
+		try {
+			AskForDenkmallisteKoeln askFor = new AskForDenkmallisteKoeln();
+			Denkmal[] denkmalArray = askFor.getDenkmallisteKoeln().result.records;
+			GsonBuilder gsonBuilder = new GsonBuilder();
+			Gson gson = gsonBuilder.create();
+			json = new StringBuilder(gson.toJson(denkmalArray));
+		} catch (MalformedURLException e) {
+			logger.error(e);
+		} catch (IOException e) {
+			logger.error(e);
+		}
+	}
 
 	@Override
 	public String getJson() {
-		return sb.toString();
+		return json.toString();
 	}
 
 }
